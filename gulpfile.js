bcefef92245ec2b3191388692437e566c78c685f
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-10-04 17:10:21 -0500

    Una prueba hacia el server con angular

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var stylus = require('gulp-stylus');
 var nib = require('nib');
 var concat = require('gulp-concat-css');
 var minify = require('gulp-minify-css'); // Es necesario porque stylus compress no me deja cambiarle el nombre
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
 
 gulp.task('styl', function () {
     return styl();
@@ -15,3 +19,14 @@ function styl() {
       .pipe(minify())
       .pipe(gulp.dest('./public'));
 }
+
+gulp.task('js', function() {
+  return browserify({
+    entries: './lib/app.js', //punto de entrada js
+  })
+  .bundle()
+  .pipe(source('app.min.js')) // archivo destino
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(gulp.dest('./public/')); // en dónde va a estar el archivo destino
+});
